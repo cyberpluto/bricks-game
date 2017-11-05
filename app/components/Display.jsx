69ed20750dbc5aabf2c33d7600000a5ef3fcbb37
Arek <arek@yaska.eu>
2017-11-05 12:58:16 +0100

    Removed column gap and replaced it with margin around pixel.

--- app/components/Display.jsx
+++ app/components/Display.jsx
@@ -1,25 +1,11 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import styled, {keyframes} from 'styled-components'
-
-const flash = keyframes`
-0% {
-	opacity: 0;
-}
-50% {
-	opacity: 1;
-}
-100% {
-	opacity: 0;
-}
-`
+import styled from 'styled-components'
 
 const DisplayWrapper = styled.div`
 	display: inline-grid;
-	grid-template-columns: repeat(7, calc(100vh / 15));
-	grid-template-rows: repeat(15, calc(100vh / 15));
-	grid-column-gap: 10px;
-	grid-row-gap: 10px;
+	grid-template-columns: repeat(7, calc(100vh / 13));
+	grid-template-rows: repeat(13, calc(100vh / 13));
 	border: 4px solid #fff;
 	overflow: hidden;
 `
@@ -29,8 +15,8 @@ const Pixel = styled.div`
 	grid-row-start: ${p => p.y};
 	grid-row-end: span 1;
 	background: red;
-	// animation: ${flash} 0.3s ease;
 	box-shadow: 0 0 6px 1px red;
+	margin: 5px;
 `
 
 class Display extends Component {
